b9f543d7ba45fa25725c17a885250c986f12b05d
eloan <eloandelamoga@gmail.com>
2021-01-20 13:36:04 +0100

    V 0.2
    Ajout de la fonctionnalité du menu

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 "use strict";
 
+const main =  document.querySelector("main");
+const icon = document.querySelector(".menu-icon");
+let canOpenMenu = true;
+
+icon.addEventListener("click", event => {
+    event.stopPropagation();
+    if(canOpenMenu) {
+        const menu = document.createElement('section');
+        menu.className = "menu";
+        menu.innerHTML = `
+        <ul>
+            <li>
+                <p><a href="">Qui sommes-nous ?</a></p>
+                <span></span>
+            </li>
+            <li>
+                <p><a href="">Nous rejoindre</a></p>
+                <span></span>
+            </li>
+            <li>
+                <p><a href="">Nous contacter</a></p>
+                <span></span>
+            </li>
+        </ul>
+        `;
+        main.append(menu);
+        canOpenMenu = !canOpenMenu;
+
+        menu.addEventListener('mouseleave', event => {
+            menu.remove();
+            canOpenMenu = !canOpenMenu;
+        });    
+    };
+
+
+});
+
