43b7d48a9a2e7f5eb97cbc1847c14672a1f0d328
eloan <eloandelamoga@gmail.com>
2021-01-21 14:05:24 +0100

    v0.4
    ajout de l'animation de fermeture du menu, changement de la police et recolorisation de l'icône du menu ainsi que du titre

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const main =  document.querySelector("main");
 const icon = document.querySelector(".menu-icon");
 const body = document.querySelector("body");
-const menu = document.querySelector(".menu");
 let canOpenMenu = true;
 
 icon.addEventListener("click", event => {
@@ -13,32 +12,32 @@ icon.addEventListener("click", event => {
 
 const displayMenu = () => {
     if(canOpenMenu) {
-        const menu = document.createElement('section');
-        menu.className = "menu";
+        const menu = document.createElement("section");
+        menu.className ="menu";
         menu.style.animation = 'menu-open 0.2s';
-        menu.innerHTML = `
+        menu.insertAdjacentHTML("afterbegin",`
         <ul>
             <li>
-                <p><a href="">Qui sommes-nous ?</a></p>
+                <p><a class="menu-text" href="">Qui sommes-nous ?</a></p>
                 <span></span>
             </li>
             <li>
-                <p><a href="">Nous rejoindre</a></p>
+                <p><a class="menu-text" href="">Nous rejoindre</a></p>
                 <span></span>
             </li>
             <li>
-                <p><a href="">Nous contacter</a></p>
+                <p><a class="menu-text" href="">Nous contacter</a></p>
                 <span></span>
             </li>
         </ul>
-        `;
+        `);
         main.append(menu);
-        canOpenMenu = !canOpenMenu;
+        canOpenMenu = !canOpenMenu
 
         body.addEventListener('click', event => {
-            menu.style.animation = 'menu-close 2s';
-            menu.remove();
-            canOpenMenu = !canOpenMenu;
+            menu.style.animation = 'menu-close 0.2s';
+            setTimeout(() => menu.remove(), 100);
+            canOpenMenu = true;
         });    
     };
 };
